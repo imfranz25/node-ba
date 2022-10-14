34f96f5c52938860bb3e774856fb650a565a58f0
Francis Ong <francis.ong25@gmail.com>
2022-10-14 16:48:38 +0800

    Add static and urlencoded config

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ require("./config/database").connect();
 
 // setup express
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // import routes
 const account = require("./routes/account");
 
-app.use(express.json());
+app.use( express.json() );
+app.use( express.urlencoded({ extended: false }) );
+app.use( express.static(path.join(__dirname, "public")) );
 app.use("/account", account);
 
 app.get("/", (req, res) => {

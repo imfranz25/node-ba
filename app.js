7bf2ec780461738fe833bd555c406833fc7b3a6c
Francis Ong <francis.ong25@gmail.com>
2022-10-15 23:41:57 +0800

    Add comments

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// environment variables and database connection - configs
 require("dotenv").config();
 require("./config/database").connect();
 
@@ -9,9 +10,9 @@ const app = express();
 // import routes
 const account = require("./routes/account");
 
+// express configs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 app.use( express.json() );
 app.use( express.urlencoded({ extended: false }) );
 app.use( express.static(path.join(__dirname, "public")) );
@@ -19,15 +20,17 @@ app.use( express.static(path.join(__dirname, "public")) );
 // API -> Account (register/login)
 app.use("/account", account);
 
-// Routes
+// Landing Page
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Login Page
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
+// Sign Up Page
 app.get("/register", (req, res) => {
   res.render("register");
 });

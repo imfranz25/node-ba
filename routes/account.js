c4eb9b93ef2b3230e9b9734a240d8181fe0b207f
Francis Ong <francis.ong25@gmail.com>
2022-10-16 13:02:25 +0800

    feat(404.js): add 404 page - for page not found

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createAccount } = require('../controllers/account');
+const { createAccount, validateUser } = require('../controllers/account');
 
 const router = express.Router();
 
-// ROUTING CALLS
+// ROUTING CALLS for /account
 router.post('/create', createAccount);
+router.post('/login', validateUser);
 
 module.exports = router;

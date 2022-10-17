e7c053391a5495d79f5d76350f0640c3837db2d9
Francis Ong <francis.ong25@gmail.com>
2022-10-17 08:49:45 +0800

    feat: add redirect if user has document cookie -> token

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+function getCookie(cname) {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+const token = getCookie('token');
+if (token) {
+  window.location.replace(`http://${window.location.host}/dashboard`);
+}

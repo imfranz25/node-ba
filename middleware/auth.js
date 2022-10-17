399bc5ca29cfd1ab1c93e389c34480b9985430bc
Francis Ong <francis.ong25@gmail.com>
2022-10-17 11:35:45 +0800

    fix: remove 301 status code in redirect

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const config = process.env;
 const verifyToken = (req, res, next) => {
   const token = req.body.token || req.query.token || req.headers['x-access-token'] || getcookie(req).token;
   if (!token) {
-    return res.redirect(301, '/login');
+    return res.redirect('/login');
   }
 
   try {
@@ -15,7 +15,7 @@ const verifyToken = (req, res, next) => {
   } catch (err) {
     res.clearCookie('token');
     res.clearCookie('username');
-    return res.redirect(301, '/login');
+    return res.redirect('/login');
   }
   return next();
 };
